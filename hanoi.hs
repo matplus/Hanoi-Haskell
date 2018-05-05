2fa8af6c06829ed0ae7604b267964517fc7e3a6b
matplus <mikoto.matplus@gmail.com>
2018-05-06 04:29:02 +0900

    Reduced side effects.

--- hanoi.hs
+++ hanoi.hs
@@ -5,19 +5,24 @@ main :: IO ()
 main = do
   args <- getArgs
   if length args == 1
-    then hanoi (read (args !! 0) :: Int)
+    then printanswer (hanoi (read (args !! 0) :: Int))
     else printf "Usage: hanoi discs\n"
 
-hanoi :: Int -> IO ()
-hanoi discs = dohanoi (discs, "from-pole", "to-pole", "work-pole")
-dohanoi :: (Int, String, String, String) -> IO ()
-dohanoi (discno, from, to, work)
-  | discno == 1 = move (discno, from, to)
-  | discno > 1 = do
-      dohanoi ((discno - 1), from, work, to)
-      move (discno, from, to)
-      dohanoi ((discno - 1), work, to, from)
-  | otherwise = undefined
+printanswer :: [String] -> IO ()
+printanswer (x:[]) =
+  printf "%s\n" x
+printanswer (x:xs) = do
+  printf "%s\n" x
+  printanswer xs
 
-move :: (Int, String, String) -> IO ()
-move (discno, from, to) = printf "Move disc %d from %s to %s.\n" discno from to
+hanoi :: Int -> [String]
+hanoi discs = dohanoi (discs, "from-pole", "to-pole", "work-pole")
+
+dohanoi :: (Int, String, String, String) -> [String]
+dohanoi (discno, from, to, work)
+  | discno == 1  = move (discno, from, to)
+  | discno > 1   = dohanoi ((discno - 1), from, work, to) ++ move (discno, from, to) ++ dohanoi ((discno - 1), work, to, from)
+  | otherwise    = undefined
+
+move :: (Int, String, String) -> [String]
+move (discno, from, to) = ["Move disc " ++ show discno ++ " from " ++ from ++ " to " ++ to ++ "."]
